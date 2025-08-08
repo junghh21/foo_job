8abed5ae8a316532486136c0b68e82cc5f8ba645
junghh21 <junghh21@gmail.com>
2025-08-08 14:42:12 +0900

    asdasads

--- api/index.py
+++ api/index.py
@@ -7,13 +7,25 @@ sys.path.append(os.path.abspath(".."))
 import app
 
 from http.server import BaseHTTPRequestHandler
-
+import json
+# Create a custom HTTP request handler
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-        
+		# Define how to handle GET requests
+		def do_GET(self):
+				print("Here => ", self.path)
+				if self.path == "/api":
+						self.send_response(200)
+						self.send_header("Content-type", "text/plain")
+						self.end_headers()
+						self.wfile.write(
+								"Python API Example - Welcome to the homepage!".encode("utf-8")
+						)
+				else:
+						self.send_response(404)
+						self.send_header("Content-type", "application/json")
+						self.end_headers()
+						error_data = {"message": "Not Found", "status": "error"}
+						json_error_data = json.dumps(error_data)
+						self.wfile.write(json_error_data.encode("utf-8"))
 #async def handler (req):
 #  app.handle_params(req)
